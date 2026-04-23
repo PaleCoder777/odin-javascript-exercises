6638b6c333ee42ee0b6f351072fc2b2c540e8400
PaleCoder777 <236280288+PaleCoder777@users.noreply.github.com>
2026-04-23 19:04:16 -0400

    Create repeatString() using loop, solve first test

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let repeatedString = ("");
 
+    for (let i=1; i <= num; i++) {
+        if (num === 0 || num < 0) return ("Error");
+        
+        repeatedString += string;
+    }
+
+    return repeatedString;
 };
+
+repeatString("hey", 3);
 
 // Do not edit below this line
 module.exports = repeatString;
