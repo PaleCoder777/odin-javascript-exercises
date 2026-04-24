9be17cee9de173bc4bc07dac1cdc9246d67d27df
PaleCoder777 <236280288+PaleCoder777@users.noreply.github.com>
2026-04-24 09:27:15 -0400

    Improv function to handle more input cases
    The functions now handles larger first argument,
    negative arguments and non-number arguments. They
    will stop the function.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -3,6 +3,18 @@ const sumAll = function(a, b) {
     let arr = [];
     let sum = 0;
     
+    // if arguments are less than 0, fail
+    if (a < 0 || b < 0) return ("ERROR");
+
+    // if arguments are float/have decimals, fail
+    if (a % 1 !== 0 || a % 1 !== 0) return ("ERROR");
+
+    // if arguments are non-numbers, fail
+    if (typeof(a) !== "number" || typeof(b) !== "number") return ("ERROR");
+    
+    //  makes it so starting with larger input doesn't matter (numbers switch if true)
+    if (a > b) [a, b] = [b, a];
+
     // add each number [1, 4] to that list
     for (let i = a; i <= b; i++) {
         arr.push(i);
