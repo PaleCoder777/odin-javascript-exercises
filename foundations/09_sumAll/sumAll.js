122c61dfeeebb05f1e8be6175428c98cc2a10c02
PaleCoder777 <236280288+PaleCoder777@users.noreply.github.com>
2026-04-24 08:59:40 -0400

    Write sumAll function
    Make a function that finds sum of [a, b]

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    // make a list of all number between the parameters (a, b)
+    let arr = [];
+    let sum = 0;
+    
+    // add each number [1, 4] to that list
+    for (let i = a; i <= b; i++) {
+        arr.push(i);
+    }
 
+    // get the sum
+    sum = arr.reduce((sum, currentNumber) => sum += currentNumber);
+    
+    return sum;
 };
 
 // Do not edit below this line
