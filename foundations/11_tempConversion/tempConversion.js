8af7e357749ed0b356e67400322d8489c4daabbf
PaleCoder777 <236280288+PaleCoder777@users.noreply.github.com>
2026-04-26 17:41:31 -0400

    Add code block for conversion functions

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFahrenheit) {
+// Temperature in degrees Celsius (°C) = (Temperature in degrees Fahrenheit (°F) - 32) * 5/9
+let tempInCelsius = (tempInFahrenheit - 32) * (5/9);
+let tempInCelsiusRounded = Math.round(tempInCelsius * 10) / 10; 
+return tempInCelsiusRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelsius) {
+// Temperature in degrees Fahrenheit (°F) = (Temperature in degrees Celsius (°C) * 9/5) + 32
+let tempInFahrenheit = (tempInCelsius * (9/5)) + 32;
+let tempInFahrenheitRounded = Math.round(tempInFahrenheit * 10) / 10;
+return tempInFahrenheitRounded;
 };
 
 // Do not edit below this line
