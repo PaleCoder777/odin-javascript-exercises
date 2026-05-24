7f09611d6d3f29e81b400ebbb8fd4a7cc25838b2
PaleCoder777 <236280288+PaleCoder777@users.noreply.github.com>
2026-05-24 18:07:15 -0400

    Add calculator functions

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+// take two numbers and return their sum
+const add = function(num1, num2) {
+	return (num1 + num2);
 };
 
-const subtract = function() {
-	
+// take two numbers and return the result after subtracting the second from the first
+const subtract = function(num1, num2) {
+	return (num1 - num2);
 };
 
-const sum = function() {
-	
+// take an array of numbers and add all the numbers together
+const sum = function(array) {
+	return array.reduce((sum, number) => (sum + number), 0);
 };
 
-const multiply = function() {
-
+// take an array of numbers and multiply all the numbers together
+const multiply = function(array) {
+	return array.reduce((product, number) => (product * number));
 };
 
-const power = function() {
-	
+// take two numbers (base and exponent) and return the result of raising the base to that exponent
+const power = function(base, exponent) {
+	return (base ** exponent);
 };
 
-const factorial = function() {
-	
+// given a single number, find its [factorial](https://en.wikipedia.org/wiki/Factorial)
+const factorial = function(num) {
+  let factorial = 1;
+
+  for (let i = num; i > 0; i--){    
+    factorial *= i;
+  }
+
+  return factorial;
 };
 
 // Do not edit below this line
