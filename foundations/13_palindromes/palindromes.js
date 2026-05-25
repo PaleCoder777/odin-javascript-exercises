7da388b104aa66363ad9800bd9b1823329049be5
PaleCoder777 <236280288+PaleCoder777@users.noreply.github.com>
2026-05-24 20:04:46 -0400

    Add function to check palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+// If normal string === reverse string TRUE, else FALSE
+const palindromes = function (string) {
+    string = string.toLowerCase().replace(/[.,!]/g, "");
+    string = string.split(" ");
+    string = string.join("");
 
+    let reverseString = "";
+    for (let i = string.length-1; i >= 0; i--) {
+        reverseString = reverseString.concat(string[i]);
+    }
+
+    return (string === reverseString);
 };
 
 // Do not edit below this line
